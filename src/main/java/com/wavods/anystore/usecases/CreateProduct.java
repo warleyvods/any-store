96a526dfb8a758fff70a4e65d33f7c3beb5b95c4
Warley <warleyvods@gmail.com>
2023-05-23 23:27:22 -0300

    removendo

--- src/main/java/com/wavods/anystore/usecases/CreateProduct.java
+++ src/main/java/com/wavods/anystore/usecases/CreateProduct.java
@@ -5,23 +5,10 @@ import com.wavods.anystore.gateways.ProductGateway;
 import com.wavods.anystore.gateways.ProductImageGateway;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
-import static java.util.Optional.ofNullable;
-
 @Component
 public record CreateProduct(ProductGateway productGateway, ProductImageGateway productImageGateway) {
 
     public Product execute(final Product product) {
-        return productGateway.save(product);
-    }
-
-    public Product execute(final Product product, final List<Long> productImageIds) {
-        ofNullable(productImageIds)
-                .filter(productImagesIds -> !productImagesIds.isEmpty())
-                .ifPresent(ids -> ids.stream().map(productImageGateway::findById)
-                        .forEach(product::addProductImages));
-
         return productGateway.save(product);
     }
 }
